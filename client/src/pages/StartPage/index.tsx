16cd3f9a53b3bc8970fe355c1699ca301680b52f
“matcs06” <you@example.com>
2023-02-19 00:19:40 -0300

    adding start page

---
+++ client/src/pages/StartPage/index.tsx
@@ -0,0 +1,32 @@
+export default function Test() {
+   return (
+      <div className="flex relative flex-col items-center text-dark-gray h-full">
+         <header className="bg-secondary-orange h-28 w-full">
+         </header>
+         <div className="w-40 absolute top-10 rounded-lg  flex flex-col justify-center items-center shadow-dark-gray shadow-md	 bg-light-gray h-28 justify-self-center">
+            Foto da Loja
+
+            <div className="w-1/2 flex text-primary-bk cursor-pointer justify-center bg-light-gree h-6 absolute top-24 rounded-lg z-10 ">
+               Aberto
+            </div>
+         </div>
+         <div className="mt-24 h-full w-full flex flex-col justify-center items-center">
+            <div className="flex  flex-row max-w-md border-b-b-1/5 border-b-light-gray-2 mb-2  cursor-pointer w-full py-3 px-2 select-none" >
+               <div className="px-5 w-3/4">
+                  <p className="text-dark-gray font-bold mb-2 ">Mousse de Chocolate</p>
+                  <p className="text-justify font-light text-sm">Delicioso Mousse de Chocolate com raspas de chocolate e limão, feito com 50% cacau. 220 ml.</p>
+                  <p className="mt-3 text-light-gree text-sm">Por apenas: <b> 12,00 R$</b></p>
+               </div>
+               <img src="./test2.png" alt="image-produto" className="rounded-lg w-28 h-28 mt-1" />
+
+
+            </div>
+
+
+
+         </div>
+
+
+      </div>
+   )
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      inter: ["Inter", "sans-serif"]
+    },
+    borderWidth: {
+      "b-1/5": "0.5px"
+    },
+    colors: {
+      "light-gray": "#F5F5F5",
+      "light-gray-2": "#D6D6D6",
+      "primary-bk": "#fff",
+      "secondary-orange": "#DC6A6A",
+      "green": "#81CB67",
+      "light-gree": "#4ED15C",
+      "dark-gray": "#605E5E",
+    }
+  },
+  plugins: [],
+}
