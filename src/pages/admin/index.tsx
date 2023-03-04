1bfee864686a235c2d3269da1f665f2c61588901
Mateus Costa Silva <38388657+matcs06@users.noreply.github.com>
2023-03-03 22:29:41 -0300

    delete dir 2

--- src/pages/admin/index.tsx
+++
@@ -1,8 +0,0 @@
-import SideBar from "../../components/SideBar/SideBar"
-import ProductList from "./Product/ProductList/ProductList"
-export default function AdminApp() {
-
-   return (
-      <SideBar defaultComponent={<ProductList />} />
-   )
-}
